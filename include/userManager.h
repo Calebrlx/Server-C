a784e9deb1e4f821b5d8aa9923e2a022d8e7293d
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-13 23:38:36 -0800

    Update userManager.h

--- include/userManager.h
+++ include/userManager.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include <string>
+#include <memory>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include "nlohmann/json.hpp"
+
+class UserManager {
+public:
+    UserManager();
+    ~UserManager();
+    bool registerUser(const nlohmann::json& userData);
+    bool loginUser(const nlohmann::json& credentials, std::string& token);
+
+private:
+    std::unique_ptr<sql::Connection> con;
+    bool connectToDatabase();
+};
