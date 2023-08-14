167de0134675013bf3f0d90b7f8c12834e21c994
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-13 17:27:54 -0800

    Update and rename jwt.h to connectionHandler.h

---
+++ include/authentication/connectionHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "nlohmann/json.hpp"
+
+class ConnectionHandler {
+public:
+    ConnectionHandler();
+    ~ConnectionHandler();
+    bool start();
+    void stop();
+
+private:
+    int server_fd;
+    int new_socket;
+    sockaddr_in address;
+    std::map<std::string, std::string> users; // Username and hashed password mapping
+    std::unordered_map<std::string, std::string> active_tokens; // Active tokens with associated usernames
+
+    std::string extractJsonBody(const std::string& request);
+    void sendJsonResponse(const nlohmann::json& json, int status_code);
+    void handleRegisterRequest(const std::string& received);
+    void handleLoginRequest(const std::string& received);
+    void sendErrorResponse(const std::string& message, int status_code);
+    void sendSuccessResponse(const std::string& message);
+    bool validateToken(const std::string& token); // Function to validate tokens
+};
