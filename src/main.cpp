02bb39a9f64c50708ad494518e3648433062d25a
Calebslc <133164966+Calebslc@users.noreply.github.com>
2023-08-13 17:17:50 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,26 @@
+#include "ConnectionHandler.h"
+#include <signal.h>
 
+volatile bool run_server = true;
+
+void handle_sigint(int sig) {
+    run_server = false;
+}
+
+int main() {
+    signal(SIGINT, handle_sigint);
+
+    ConnectionHandler server;
+
+    if (!server.start()) {
+        std::cerr << "Server failed to start\n";
+        return -1;
+    }
+
+    while (run_server) {
+        // Server processing logic
+    }
+
+    server.stop();
+    return 0;
+}
